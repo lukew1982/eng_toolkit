83651a9f17b3225661d8d88e73577f6e8117f9b4
Luke Whitehead <lukew1982@gmail.com>
2015-03-17 00:04:40 +0000

    Added error messages and formatted result to always include double digits

--- scripts/enghours.js
+++ scripts/enghours.js
@@ -4,7 +4,16 @@ $(document).ready(function() {
         var inhrs = (parseInt($('#afhrs1').val() * 60)) + parseInt($('#afmins1').val());
         var ecuhrs = (parseInt($('#ecuhrs1').val() * 60)) + parseInt($('#ecumins1').val());
         var result = afhrs - inhrs + ecuhrs;
-        $('#result').text(Math.floor(result / 60) + ' : ' + result % 60).addClass('res1');
+        
+        if (isNaN(result)) {
+            $('#result').text('Error: You have entered a value that is not a number.').removeClass('highlight').addClass('error');
+        } else if (afhrs  < inhrs) {
+            $('#result').text('Error: A/F hours on installation cannot be greater than current A/F hours.').removeClass('highlight').addClass('error');
+        } else  if (($('#curmins').val()) > 59 || ($('#afmins1').val()) > 59 || ($('#ecumins1').val()) > 59) {
+            $('#result').text('Error: The value of the minutes field cannot be greater than 59.').removeClass('highlight').addClass('error');
+        } else {
+            $('#result').text(Math.floor(result / 60) + ' : ' + ("0" + (result % 60)).slice(-2)).removeClass('error').addClass('highlight');
+        }
     });
     $('#reset').on('click', function() {
         $('#afhrs1').val('');
